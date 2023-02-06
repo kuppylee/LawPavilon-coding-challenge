122a2ce20698616d19ffe116c4f3ca7383de8cb6
kuppylee <bjkuppy@gmail.com>
2023-02-06 08:27:08 +0100

    Fix: Fixed the issue with the api url breaking the code by using a guard let instead of force unwrapping

--- lawPavillonCodingChallenge/lawPavillonCodingChallenge/resources/DataProvider.swift
+++ lawPavillonCodingChallenge/lawPavillonCodingChallenge/resources/DataProvider.swift
@@ -13,7 +13,7 @@ struct FetchSearchResult {
     func fetchSearchResult(page: Int, perPage: Int, username: String, completionHandler: @escaping (_ result: SearchResultResponse?,_ statusCode:Int) -> Void) {
         let serviceProvider = ServiceProvider()
         let searchResultUrl = ApiEndpoint.userEndpoint + "?q=\(username)&page=\(page)&per_page=\(perPage)"
-        let url = URL(string: searchResultUrl)!
+        guard let url = URL(string: searchResultUrl) else {return}
         do {
             serviceProvider.fetchAPIDataAndStatusCode(requestURL: url, resultType: SearchResultResponse.self) { result, statusCode in
                 completionHandler(result,statusCode)
